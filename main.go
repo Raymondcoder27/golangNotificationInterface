a067d4209bf3feaab036d3c75ccf63f46d73a554
MwebeCoder27 <mwebraymond13@gmail.com>
2024-09-11 13:32:24 +0300

    init commit

--- main.go
+++ main.go
@@ -1,6 +1,10 @@
 package main
 
-import "net/http"
+import (
+	"encoding/json"
+	"log/slog"
+	"net/http"
+)
 
 type Account struct {
 	Username string
@@ -8,5 +12,9 @@ type Account struct {
 }
 
 func handleCreateAccount(w http.ResponseWriter, r http.Request) {
-
+	var account Account
+	if err := json.NewDecoder(r.Body).Decode(&account); err != nil {
+		slog.Error("Failed to decode the response body", "err", err)
+		return
+	}
 }
